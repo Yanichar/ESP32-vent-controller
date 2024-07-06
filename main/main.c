6a861ba83d45f71ae57e6f6ebf74316e03ab99c1
Ivan Sytnik <i.sytnik@cleack.com>
2024-07-06 16:58:37 +0300

    Refactor

--- main/main.c
+++ main/main.c
@@ -3,19 +3,18 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-// Определяем выводы
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_NUM_5) | (1ULL<<GPIO_NUM_18) | (1ULL<<GPIO_NUM_19) | (1ULL<<GPIO_NUM_21))
 
-void set_inverted_gpio_values(uint8_t value) {
-    // Инвертируем значения и устанавливаем на выводы
+void set_inverted_gpio_values(uint8_t value)
+{
     gpio_set_level(GPIO_NUM_5, !(value & 0x01));
     gpio_set_level(GPIO_NUM_18, !(value & 0x02));
     gpio_set_level(GPIO_NUM_19, !(value & 0x04));
     gpio_set_level(GPIO_NUM_21, !(value & 0x08));
 }
 
-void app_main(void) {
-    // Настройка выводов
+void app_main(void)
+{
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
@@ -24,13 +23,12 @@ void app_main(void) {
     io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
 
-    while(1) {
-        for (uint8_t i = 0; i < 16; i++) {
-            // Выводим число от 0 до 15 в порт
+    while(1)
+    {
+        for (uint8_t i = 0; i < 16; i++)
+        {
             set_inverted_gpio_values(i);
-
-            // Задержка 0.5 секунды
-            vTaskDelay(pdMS_TO_TICKS(500));
+            vTaskDelay(pdMS_TO_TICKS(5000));
         }
     }
 }
