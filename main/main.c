1026878bcf48ab6b92f2dc972ad14ffefbeb95a9
Ivan Sytnik <i.sytnik@cleack.com>
2024-07-06 16:56:34 +0300

    Make output inverse

--- main/main.c
+++ main/main.c
@@ -6,8 +6,15 @@
 // Определяем выводы
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_NUM_5) | (1ULL<<GPIO_NUM_18) | (1ULL<<GPIO_NUM_19) | (1ULL<<GPIO_NUM_21))
 
-void app_main(void)
-{
+void set_inverted_gpio_values(uint8_t value) {
+    // Инвертируем значения и устанавливаем на выводы
+    gpio_set_level(GPIO_NUM_5, !(value & 0x01));
+    gpio_set_level(GPIO_NUM_18, !(value & 0x02));
+    gpio_set_level(GPIO_NUM_19, !(value & 0x04));
+    gpio_set_level(GPIO_NUM_21, !(value & 0x08));
+}
+
+void app_main(void) {
     // Настройка выводов
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_INTR_DISABLE;
@@ -17,33 +24,10 @@ void app_main(void)
     io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
 
-    // Комбинации для перебора
-    uint8_t combinations[16][4] = {
-            {0, 0, 0, 0},
-            {0, 0, 0, 1},
-            {0, 0, 1, 0},
-            {0, 0, 1, 1},
-            {0, 1, 0, 0},
-            {0, 1, 0, 1},
-            {0, 1, 1, 0},
-            {0, 1, 1, 1},
-            {1, 0, 0, 0},
-            {1, 0, 0, 1},
-            {1, 0, 1, 0},
-            {1, 0, 1, 1},
-            {1, 1, 0, 0},
-            {1, 1, 0, 1},
-            {1, 1, 1, 0},
-            {1, 1, 1, 1}
-    };
-
     while(1) {
-        for (int i = 0; i < 16; i++) {
-            // Установка значений на выводы
-            gpio_set_level(GPIO_NUM_5, combinations[i][0]);
-            gpio_set_level(GPIO_NUM_18, combinations[i][1]);
-            gpio_set_level(GPIO_NUM_19, combinations[i][2]);
-            gpio_set_level(GPIO_NUM_21, combinations[i][3]);
+        for (uint8_t i = 0; i < 16; i++) {
+            // Выводим число от 0 до 15 в порт
+            set_inverted_gpio_values(i);
 
             // Задержка 0.5 секунды
             vTaskDelay(pdMS_TO_TICKS(500));
